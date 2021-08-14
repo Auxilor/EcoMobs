cafb41567f68903cf70f8085e0bf48e5f3754f9c
Auxilor <william.favierparsons1@gmail.com>
2021-08-14 13:19:28 +0100

    Added /ecobosses give

--- eco-core/core-plugin/src/main/java/com/willfp/ecobosses/commands/CommandEcobosses.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecobosses/commands/CommandEcobosses.java
@@ -16,7 +16,8 @@ public class CommandEcobosses extends PluginCommand {
         this.addSubcommand(new CommandReload(plugin))
                 .addSubcommand(new CommandKillall(plugin))
                 .addSubcommand(new CommandSpawn(plugin))
-                .addSubcommand(new CommandBase64(plugin));
+                .addSubcommand(new CommandBase64(plugin))
+                .addSubcommand(new CommandGive(plugin));
     }
 
     @Override
