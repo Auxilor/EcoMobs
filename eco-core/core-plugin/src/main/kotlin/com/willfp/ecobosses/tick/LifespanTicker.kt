64b22a17d6c5aae0ddd40de2f9c23a7c6dcd3f2e
Auxilor <william.favierparsons1@gmail.com>
2022-02-06 13:30:30 +0000

    Fixed lifespan

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/tick/LifespanTicker.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/tick/LifespanTicker.kt
@@ -1,13 +1,18 @@
 package com.willfp.ecobosses.tick
 
 import com.willfp.ecobosses.bosses.LivingEcoBoss
+import com.willfp.ecobosses.events.BossDespawnEvent
 import com.willfp.ecobosses.lifecycle.BossLifecycle
+import org.bukkit.Bukkit
 
 class LifespanTicker : BossTicker {
     override fun tick(boss: LivingEcoBoss, tick: Int) {
         val timeLeft = (boss.deathTime - System.currentTimeMillis()) / 1000.0
 
         if (timeLeft <= 0) {
+            val event = BossDespawnEvent(boss)
+            Bukkit.getPluginManager().callEvent(event)
+
             boss.remove()
             boss.boss.handleLifecycle(
                 BossLifecycle.DESPAWN,
