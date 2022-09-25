1eee00b88ea5802b0818b8057a87672ffe85e3bc
DaRacci <racci@racci.dev>
2022-09-25 23:33:24 +1000

    fix: Publishing
    Also converted gradle build to kotlin.
    Also updated gradle to 7.5.1
    
    Closes: https://github.com/Auxilor/EcoBosses/issues/49, https://github.com/Auxilor/EcoBosses/issues/41

---
+++ eco-core/core-plugin/build.gradle.kts
@@ -0,0 +1,8 @@
+group = "com.willfp"
+version = rootProject.version
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("com.github.lokka30:LevelledMobs:3.1.4")
+    compileOnly("net.kyori:adventure-api:4.9.3")
+}
