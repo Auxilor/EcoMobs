bc73201ec52086ab0ee3a9294bd3b27ba3ae8163
Auxilor <william.favierparsons1@gmail.com>
2022-10-13 20:13:11 +0100

    libreforge-updater

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/commands/CommandEcobosses.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobosses/commands/CommandEcobosses.kt
@@ -2,6 +2,10 @@ package com.willfp.ecobosses.commands
 
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.ecobosses.EcoBossesPlugin
+import com.willfp.ecobosses.bosses.Bosses
+import com.willfp.libreforge.lrcdb.CommandExport
+import com.willfp.libreforge.lrcdb.CommandImport
+import com.willfp.libreforge.lrcdb.ExportableConfig
 import org.bukkit.command.CommandSender
 
 class CommandEcobosses(plugin: EcoBossesPlugin) : PluginCommand(
@@ -22,5 +26,14 @@ class CommandEcobosses(plugin: EcoBossesPlugin) : PluginCommand(
             .addSubcommand(CommandKillall(plugin))
             .addSubcommand(CommandSpawn(plugin))
             .addSubcommand(CommandGive(plugin))
+            .addSubcommand(CommandImport("bosses", plugin))
+            .addSubcommand(CommandExport(plugin) {
+                Bosses.values().map {
+                    ExportableConfig(
+                        it.id,
+                        it.config
+                    )
+                }
+            })
     }
 }
