e15366e1b2c261769e4a3623a445d671528a2c81
Auxilor <william.favierparsons1@gmail.com>
2023-11-12 13:26:53 +0000

    Added 'none' spawn method

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomobs/category/spawning/SpawnMethodFactories.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomobs/category/spawning/SpawnMethodFactories.kt
@@ -1,10 +1,12 @@
 package com.willfp.ecomobs.category.spawning
 
 import com.willfp.eco.core.registry.Registry
+import com.willfp.ecomobs.category.spawning.impl.SpawnMethodFactoryNone
 import com.willfp.ecomobs.category.spawning.impl.SpawnMethodFactoryReplace
 
 object SpawnMethodFactories : Registry<SpawnMethodFactory>() {
     init {
         register(SpawnMethodFactoryReplace)
+        register(SpawnMethodFactoryNone)
     }
 }
---
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomobs/category/spawning/impl/SpawnMethodFactoryNone.kt
@@ -0,0 +1,33 @@
+package com.willfp.ecomobs.category.spawning.impl
+
+import com.willfp.eco.core.EcoPlugin
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.eco.core.entities.Entities
+import com.willfp.ecomobs.category.MobCategory
+import com.willfp.ecomobs.category.spawning.SpawnMethod
+import com.willfp.ecomobs.category.spawning.SpawnMethodFactory
+import com.willfp.ecomobs.mob.SpawnReason
+import com.willfp.libreforge.enumValueOfOrNull
+import org.bukkit.entity.EntityType
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.CreatureSpawnEvent
+
+object SpawnMethodFactoryNone : SpawnMethodFactory("replace") {
+    override fun create(category: MobCategory, config: Config, plugin: EcoPlugin): SpawnMethod {
+        return SpawnMethodNone(category, config, plugin)
+    }
+
+    class SpawnMethodNone(
+        category: MobCategory, config: Config, plugin: EcoPlugin
+    ) : SpawnMethod(category, config, plugin) {
+        override fun onStart() {
+
+        }
+
+        override fun onStop() {
+
+        }
+    }
+}
