ff8b48587f96cf74311ca2d817fa3ac4529d6e62
Penal Buffalo <buffalo246@outlook.com>
2024-05-24 09:41:28 -0500

    LevelledMobs 4.0 Support
    Updated the API to 4.0

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ version = rootProject.version
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-    compileOnly("com.github.lokka30:LevelledMobs:3.1.4")
+    compileOnly("io.github.arcaneplugins:levelledmobs-plugin:4.0.2")
     compileOnly("LibsDisguises:LibsDisguises:10.0.38")
     implementation("com.willfp:ModelEngineBridge:1.0.0")
 }
--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomobs/integrations/levelledmobs/IntegrationLevelledMobs.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomobs/integrations/levelledmobs/IntegrationLevelledMobs.kt
@@ -3,7 +3,7 @@ package com.willfp.ecomobs.integrations.levelledmobs
 import com.willfp.eco.core.integrations.Integration
 import com.willfp.ecomobs.integrations.MobIntegration
 import com.willfp.ecomobs.mob.impl.ecoMob
-import me.lokka30.levelledmobs.events.MobPreLevelEvent
+import io.github.arcaneplugins.levelledmobs.events.MobPreLevelEvent
 import org.bukkit.entity.Mob
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
