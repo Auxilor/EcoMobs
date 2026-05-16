edbc7e552647c3cfe5398e164991bfbed4f0c88d
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-16 01:38:03 +0100

    fix: Update ModelEngineBridge to version 1.4.0

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
     compileOnly("io.github.arcaneplugins:levelledmobs-plugin:4.0.2")
     compileOnly("me.libraryaddict.disguises:libsdisguises:11.0.14")
-    implementation("com.willfp:ModelEngineBridge:1.3.0")
+    implementation("com.willfp:ModelEngineBridge:1.4.0")
 }
 
 tasks {
